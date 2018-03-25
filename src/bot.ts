9b0c124fb164d036659e99bac60a73a1df4eb8e1
DrunkenPoney_Qc <drunkenponey@gmail.com>
2018-03-25 05:42:39 -0400

    commit

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,17 @@
-require('dotenv').config();
+import {Client} from 'discord.js';
+import chalk from 'chalk';
 
+const client = new Client();
 
+client.on('ready', (...args: any[]) => {
+    console.log(chalk`{greenBright Logged in as {blueBright ${client.user.tag}}!}`);
+});
+
+client.on('message', msg => {
+    console.log(chalk`{cyan Message received: {white ${msg.content}}}`);
+});
+
+client.login(process.env.BOT_TOKEN).catch(err => {
+    console.log(chalk.red(err));
+    process.exit(1);
+});
